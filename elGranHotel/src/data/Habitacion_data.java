86c384272b8ba5d880583bd749ca114048abf2b6
FanoDonnini <fanobahiense@hotmail.com>
2023-10-06 12:23:13 -0300

    Metodos HabData

--- elGranHotel/src/data/Habitacion_data.java
+++ elGranHotel/src/data/Habitacion_data.java
@@ -13,21 +13,24 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 public class Habitacion_data {
-    private Connection conexion=null;
+
+    private Connection conexion = null;
     private Habitacion h = new Habitacion();
     private TipoHabitacion th = new TipoHabitacion();
-    
-    public Habitacion_data(Conexion con){
-    
-        this.conexion=con.buscarConexion();
-        
+
+    public Habitacion_data(Conexion con) {
+
+        this.conexion = con.buscarConexion();
+
     }
-    
-    public List listarHabitaciones(){
-        List  <Habitacion> habitacionList = new ArrayList<>();
+
+    public List listarHabitaciones() {
+        List<Habitacion> habitacionList = new ArrayList<>();
         String sql = "SELECT * FROM habitacion h JOIN tipohabitacion th ON (h.idTipoHabitacion = th.idTipoHabitacion) ORDER BY h.idHabitacion";
         try {
             PreparedStatement ps = conexion.prepareStatement(sql);
@@ -52,50 +55,47 @@ public class Habitacion_data {
             JOptionPane.showMessageDialog(null, "Error de sentencia");
         }
 
-        return habitacionList;     
-        
-     
+        return habitacionList;
+
     }
-    
-    
-    public void agregarHabitacion(TipoHabitacion tipo, int nro, int piso, boolean ocupada){
-        
+
+    public void agregarHabitacion(TipoHabitacion tipo, int nro, int piso, boolean ocupada) {
+
         String sql = "INSERT INTO habitacion (idTipoHabitacion, nroHabitacion, piso, ocupada) VALUES (?,?,?,?);";
-        
+
         try {
-            PreparedStatement ps = conexion.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
-            ps.setInt(1,tipo.getIdTipoHabitacion());
-            ps.setInt(2,nro);
-            ps.setInt(3,piso);
-            ps.setBoolean(4,ocupada);
+            PreparedStatement ps = conexion.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, tipo.getIdTipoHabitacion());
+            ps.setInt(2, nro);
+            ps.setInt(3, piso);
+            ps.setBoolean(4, ocupada);
             ps.executeUpdate();
             ResultSet rs = ps.getGeneratedKeys();
             if (rs.next()) {
-                h.setIdHabitacion(rs.getInt(1));           
-            }else{
-                JOptionPane.showMessageDialog(null,"Error al obtener el ID");
-            }           
+                h.setIdHabitacion(rs.getInt(1));
+            } else {
+                JOptionPane.showMessageDialog(null, "Error al obtener el ID");
+            }
             ps.close();
         } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null,"Error de sentencia");
+            JOptionPane.showMessageDialog(null, "Error de sentencia");
         }
-    }    
-    
+    }
 
-    public Habitacion buscarHabitacion(int nro, int piso){
-                
+    public Habitacion buscarHabitacion(int nro, int piso) {
+
         String sql = "SELECT * FROM habitacion h JOIN tipoHabitacion th ON (h.idTipoHabitacion = th.idTipoHabitacion) WHERE nroHabitacion = ? AND piso = ?";
-        
+
         try {
             PreparedStatement ps = conexion.prepareStatement(sql);
             ps.setInt(1, nro);
             ps.setInt(2, piso);
             ResultSet rs = ps.executeQuery();
-            while(rs.next()){                
-                h.setIdHabitacion(rs.getInt("idHabitacion"));                
+            while (rs.next()) {
+                h.setIdHabitacion(rs.getInt("idHabitacion"));
                 h.setNroHabitacion(rs.getInt("nroHabitacion"));
-                h.setPiso(rs.getInt("piso"));                
-                h.setOcupada(rs.getBoolean("ocupada"));                
+                h.setPiso(rs.getInt("piso"));
+                h.setOcupada(rs.getBoolean("ocupada"));
                 th.setTipo(rs.getString("tipo"));
                 th.setCantidadCamas(rs.getInt("cantidadCamas"));
                 th.setCantidadPersonas(rs.getInt("cantidadPersonas"));
@@ -106,32 +106,32 @@ public class Habitacion_data {
             ps.close();
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Error de sentencia");
-        }        
+        }
         return h;
     }
-    
-    public void modificarHabitacion(TipoHabitacion tipo, int nro, int piso, boolean ocupada){
-        
-        String sql="UPDATE habitacion SET idTipoHabitacion=?, ocupada=? WHERE nroHabitacion=? AND piso=?";
-        
+
+    public void modificarHabitacion(TipoHabitacion tipo, int nro, int piso, boolean ocupada) {
+
+        String sql = "UPDATE habitacion SET idTipoHabitacion=?, ocupada=? WHERE nroHabitacion=? AND piso=?";
+
         try {
-            PreparedStatement ps = conexion.prepareStatement(sql);            
+            PreparedStatement ps = conexion.prepareStatement(sql);
             ps.setInt(1, tipo.getIdTipoHabitacion());
             ps.setBoolean(2, ocupada);
             ps.setInt(3, nro);
-            ps.setInt(4, piso);            
+            ps.setInt(4, piso);
             ps.executeUpdate();
             JOptionPane.showMessageDialog(null, "La habitacion ha sido actualizada");
             ps.close();
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Error de sentencia");
-        }    
+        }
     }
 
-    public void modificarDisponibilidad(int nro, int piso, boolean ocupada){
-        
-        String sql="UPDATE habitacion SET ocupada=? WHERE nroHabitacion=? AND piso=?";
-        
+    public void modificarDisponibilidad(int nro, int piso, boolean ocupada) {
+
+        String sql = "UPDATE habitacion SET ocupada=? WHERE nroHabitacion=? AND piso=?";
+
         try {
             PreparedStatement ps = conexion.prepareStatement(sql);
             ps.setBoolean(1, ocupada);
@@ -144,13 +144,13 @@ public class Habitacion_data {
             JOptionPane.showMessageDialog(null, "Error de sentencia");
         }
     }
-    
-    public void eliminarHabitacion(int nro, int piso){    
 
-        String sql="DELETE FROM habitacion WHERE nroHabitacion=? AND piso=?";
-        
+    public void eliminarHabitacion(int nro, int piso) {
+
+        String sql = "DELETE FROM habitacion WHERE nroHabitacion=? AND piso=?";
+
         try {
-            PreparedStatement ps=conexion.prepareStatement(sql);
+            PreparedStatement ps = conexion.prepareStatement(sql);
             ps.setInt(1, nro);
             ps.setInt(2, piso);
             ps.executeUpdate();
@@ -158,10 +158,57 @@ public class Habitacion_data {
             ps.close();
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Error. La habitacion se encuentra vinculada");
-        } catch (Exception e){
-          JOptionPane.showMessageDialog(null, "Error");
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error");
         }
-        
+
+    }
+
+    public List listarPorCategoria(String tipo) {
+        List<Habitacion> lista = new ArrayList();
+        String sql = "SELECT * FROM habitacion h JOIN tipohabitacion th ON (h.idTipoHabitacion=th.idTipoHabitacion) WHERE th.Tipo=?";
+
+        try {
+
+            PreparedStatement ps = conexion.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, tipo);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                h = null;
+                th = null;
+                h.setIdHabitacion(rs.getInt("idHabitacion"));
+                h.setNroHabitacion(rs.getInt("nroHabitacion"));
+                h.setPiso(rs.getInt("piso"));
+                h.setOcupada(rs.getBoolean("ocupada"));
+
+                th.setIdTipoHabitacion(rs.getInt("idTipoHabitacion"));
+                th.setTipo(rs.getString("tipo"));
+                th.setCantidadPersonas(rs.getInt("cantidadPersonas"));
+                th.setCantidadCamas(rs.getInt("cantidadCamas"));
+                th.setTipoCamas(rs.getString("tipoCamas"));
+                th.setPrecioPorNoche(rs.getDouble("precioPorNoche"));
+
+                h.setIdTipoHabitacion(th);
+                lista.add(h);
+
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error de sentencia");
+
+        }
+
+        return lista;
+
     }
     
+    public int contadorHabitXCategoria (String tipo){
+        List<Habitacion> lista = listarHabitaciones();
+        int cont=0;
+       for(int i = 0 ; i <= lista.size()-1 ; i++ ){
+           if(tipo==lista.get(i).getIdTipoHabitacion().getTipo()){
+               cont++;
+           }
+       }
+        return cont;
+    }
 }
